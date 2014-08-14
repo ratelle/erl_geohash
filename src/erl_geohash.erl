d9d00c3dddaedf7eb6c331fdde5a89a6e6fc978b
Jeremie Lasalle Ratelle <jeremie.ratelle@gmail.com>
2014-08-14 13:56:12 -0400

    Add visualization template and json conversion

--- src/erl_geohash.erl
+++ src/erl_geohash.erl
@@ -7,25 +7,31 @@
     hashes_to_term/1,
     point_in_hashes/3,
     debug_radius/4,
-    debug_radiuses/2
+    debug_radiuses/2,
+    hashes_to_rectangles/1,
+    random_circles/7,
+    visualize/2,
+    visualize_hashes/2
 ]).
 
 -on_load(init/0).
 
 -spec init() -> ok.
 init() ->
-    PrivDir = case code:priv_dir(?MODULE) of
-                  {error, _} ->
-                      EbinDir = filename:dirname(code:which(?MODULE)),
-                      AppPath = filename:dirname(EbinDir),
-                      filename:join(AppPath, "priv");
-                  Dir -> Dir
-              end,
-    SoName = filename:join(PrivDir, "geohash_nif"),
+    SoName = filename:join(priv_dir(), "geohash_nif"),
     case catch erlang:load_nif(SoName,[]) of
         ok -> ok;
         LoadError -> error_logger:error_msg("erl_geohash: error loading NIF (~p): ~p",
                                             [SoName, LoadError])
+    end.
+
+priv_dir() ->
+    case code:priv_dir(?MODULE) of
+        {error, _} ->
+            EbinDir = filename:dirname(code:which(?MODULE)),
+            AppPath = filename:dirname(EbinDir),
+            filename:join(AppPath, "priv");
+        Dir -> Dir
     end.
 
 geo_radius_hashes(_Lat, _Long, _Distance, _Iterations) ->
@@ -42,10 +48,53 @@ point_in_hashes(_Lat, _Lon, _Hashes) ->
 
 hashes_to_term(Hashes) ->
     Lst = nif_hashes_to_term(Hashes),
-    [{<<Value:64>>,Offset,Lat,Long} || {Value,Offset,Lat,Long} <- Lst].
+    [{<<Value:64>>,Offset,LL,UR} || {Value,Offset,LL,UR} <- Lst].
+
+hashes_to_rectangles(Hashes) ->
+    Terms = hashes_to_term(Hashes),
+    [{LL, UR} || {_, _, LL, UR} <- Terms].
 
 debug_radius(Lat, Long, Distance, Iterations) ->
     hashes_to_term(geo_radius_hashes(Lat, Long, Distance, Iterations)).
 
 debug_radiuses(Radiuses, Iterations) ->
     hashes_to_term(geo_radiuses_hashes(Radiuses, Iterations)).
+
+rectangles_to_ejson(Rectangles) ->
+    [{[{<<"ll">>, {[{<<"lat">>, LlLat}, {<<"lon">>, LlLon}]}},
+       {<<"ur">>, {[{<<"lat">>, UrLat}, {<<"lon">>, UrLon}]}}]} || {{LlLon, LlLat}, {UrLon, UrLat}} <- Rectangles].
+
+circles_to_ejson(Circles) ->
+    [{[{<<"lat">>, Lat}, {<<"lon">>, Lon}, {<<"radius">>, Radius}]} || {Lat, Lon, Radius} <- Circles].
+
+ejson_struct(Circles, Rectangles) ->
+    {[{<<"circles">>, circles_to_ejson(Circles)}, {<<"rectangles">>, rectangles_to_ejson(Rectangles)}]}.
+
+visualize(Circles, Rectangles) ->
+    EJson = ejson_struct(Circles, Rectangles),
+    Json = jiffy:encode(EJson),
+    Priv = priv_dir(),
+    {ok, Content} = file:read_file(filename:join(Priv, "visualize.html.template")),
+    NewContent = binary:replace(Content, <<"__GEOHASH_ELEMS__">>, Json),
+    file:write_file(filename:join(Priv, "visualize.html"), NewContent).
+
+visualize_hashes(Circles, Iterations) ->
+    Hashes = geo_radiuses_hashes(Circles, Iterations),
+    Rectangles = hashes_to_rectangles(Hashes),
+    visualize(Circles, Rectangles).
+
+random_circles(N, MinLat, MaxLat, MinLong, MaxLong, MinRadius, MaxRadius) ->
+    random_circles(N, MinLat, MaxLat, MinLong, MaxLong, MinRadius, MaxRadius, []).
+
+random_circles(0, _, _, _, _, _, _, Accum) ->
+    Accum;
+random_circles(N, MinLat, MaxLat, MinLong, MaxLong, MinRadius, MaxRadius, Accum) ->
+    Radius = {random_float(MinLat, MaxLat),
+              random_float(MinLong, MaxLong),
+              random_float(MinRadius, MaxRadius)},
+    random_circles(N-1, MinLat, MaxLat, MinLong, MaxLong, MinRadius, MaxRadius, [Radius | Accum]).
+
+random_float(Min, Max) ->
+    Range = Max - Min,
+    Add = Range * random:uniform(),
+    Min + Add.
